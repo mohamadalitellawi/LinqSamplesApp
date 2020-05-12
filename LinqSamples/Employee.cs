d109d5adeb24bb8ff07bcfca7083f465cbd69ec9
mohamad ali <mohamadalitellawi@gmail.com>
2020-05-12 23:08:29 +0300

    Add project files.

---
+++ LinqSamples/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqSamples
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
