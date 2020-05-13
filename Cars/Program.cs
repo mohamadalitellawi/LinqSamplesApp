31536a8724c08c6ba15b10464b29e5dcb78b9dd8
mohamad ali <mohamadalitellawi@gmail.com>
2020-05-14 00:18:51 +0300

    more filter and select many

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -9,7 +9,7 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            
+
             var cars = ProcessFile("fuel.csv");
 
             var query = cars.OrderByDescending(c => c.Combined)
@@ -26,12 +26,46 @@ namespace Cars
                          where car.Manufacturer == "BMW"
                          orderby car.Combined descending, car.Name ascending
                          select car;
-                         
+
 
             foreach (var car in query2.Take(20))
             {
                 Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
             }
+
+            Console.Clear();
+
+            var asking1 = cars.Any(c => c.Manufacturer == "Ford");
+            var asking2 = cars.All(c => c.Manufacturer == "BMW");
+
+            Console.WriteLine(asking1);
+            Console.WriteLine(asking2);
+
+
+            Console.Clear();
+
+
+            // flatting operator
+
+            var result = cars.Select(c => c.Name);
+
+            foreach (var name in result)
+            {
+                foreach (var letter in name)
+                {
+                    Console.WriteLine(letter);
+                }
+            }
+
+            Console.Clear();
+
+            // same result of the above
+            var result2 = cars.SelectMany(c => c.Name).OrderBy(c => c);
+            foreach (var letter in result2)
+            {
+                Console.WriteLine(letter);
+            }
+
         }
 
         private static List<Car> ProcessFile(string path)
