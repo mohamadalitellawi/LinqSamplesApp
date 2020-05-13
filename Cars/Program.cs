dc3f034da6e16d9738c84d8a20f5c1f140b8e099
mohamad ali <mohamadalitellawi@gmail.com>
2020-05-13 23:28:23 +0300

    update filtering

---
+++ Cars/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var cars = ProcessFile("fuel.csv");
+
+            var query = cars.OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name);
+
+            foreach (var car in query.Take(20))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
+
+            Console.Clear();
+
+            var query2 = from car in cars
+                         where car.Manufacturer == "BMW"
+                         orderby car.Combined descending, car.Name ascending
+                         select car;
+                         
+
+            foreach (var car in query2.Take(20))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            var results = new List<Car>();
+
+            results = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv)
+                .ToList();
+
+            return results;
+        }
+    }
+}
--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqSamples
 {
@@ -6,7 +8,37 @@ namespace LinqSamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable< Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee {Id = 2 , Name = "Chris"}
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id=3, Name = "Alex"}
+            };
+
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            //Func<int, int> square = x => x * x;
+
+            //Console.WriteLine($"Square Value Of {5} = {square(5)}");
+
+            var query = from developer in developers
+                        where developer.Name.Length == 5 && developer.Id >= 1
+                        orderby developer.Name descending
+                        select new { Name = developer.Name};
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
+
     }
 }
